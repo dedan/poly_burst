3a99cb1879daafadc58d017ec2a917b008db1e80
Stephan Gabler <stephan.gabler@gmail.com>
2011-08-18 16:02:33 +0200

    fixed the clipping problem
    I had the problem that when I saved an image from opengl, the saved image showed a wider angle then the one shown in GLUT. 
    
    the solution was easy, I only had to swap two lines
    
    also added loading of the mona lisa and a fitness function

--- genetics/scratch.py
+++ genetics/scratch.py
@@ -12,17 +12,15 @@ from numpy.random import random
 import pool
 
 
-width = 640
-height = 480
-
-d = pool.Drawing(width, height)
+def fitness(im1, im2):
+    """docstring for fitness"""
+    return np.sum((im1-im2)**2)
 
 def DrawStuff():
 
     global d
-    for i in range(40):
+    for i in range(1):
         glClear(GL_COLOR_BUFFER_BIT)
-        glPushMatrix()
 
         for poly in d.polies:
             glColor4f(*poly.color)
@@ -34,20 +32,30 @@ def DrawStuff():
                 glVertex2f(poly.points[0][0], poly.points[0][1])
             glEnd() # GL_POLYGON
 
-        glPopMatrix ()
-        glutSwapBuffers ()
         glPixelStorei(GL_PACK_ALIGNMENT, 1)
         data = glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE)
-        image = Image.fromstring("RGBA", (width, height), data)
-        image.save('%d.png' % i, "PNG")
-        print 'bla'
+        # TODO maybe use frombuffer here, check what is faster
+        image = Image.fromstring("RGBA", (width, height), data).convert('RGB')
+        image.show()
+        im_ar = np.asarray(image)
         d.mutate()
+        glutSwapBuffers()
+        
+
+
+ml = Image.open('ml.bmp')
+ml_ar = np.asarray(ml)
+width = ml.size[0]
+height = ml.size[1]
+
+d = pool.Drawing(width, height)
+
 
 # glut initialization
 glutInit(sys.argv)
-glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB)
-glutCreateWindow("Draw Polygons")
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA)
 glutInitWindowSize(width, height)
+glutCreateWindow("Draw Polygons")
 
 # set the function to draw
 glutDisplayFunc(DrawStuff)
@@ -62,7 +70,6 @@ glLoadIdentity()
 glOrtho(0, width, height, 0, 0, 1)
 glDisable(GL_DEPTH_TEST)
 glMatrixMode(GL_MODELVIEW)
-
 
 
 
