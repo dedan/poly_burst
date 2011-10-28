75b9e3bdec81e28d9896ff00a98fb2d4ba3d98ce
Stephan Gabler <stephan.gabler@gmail.com>
2011-10-28 11:13:42 +0200

    replace polygon object by simple dictionary
    * because I removed in the last step all the logic from the polygon class I can now simply store all the information in a dictionary
    * this also makes serialization to json super simple
    * json serialization is done that luis later can use the polygons

--- genetics/pool.py
+++ genetics/pool.py
@@ -3,23 +3,21 @@ from random import choice
 from numpy.random import random, normal, uniform, randint
 import numpy as np
 import cairo
-import copy
 import logging
+import json
+import copy
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%m-%d %H:%M')
 
 
-class Polygon(object):
-    """a random polygon"""
-
-    def __init__(self, width, height, n_points):
-        super(Polygon, self).__init__()
-        self.points = []
-        for _ in range(n_points):
-            self.points.append((randint(0, width), randint(0, height)))
-        self.color = (random(), random(), random(), uniform(0.3, 0.6))
+def create_random_poly(width, height, n_points):
+    """create a random polygon in the given range"""
+    points = zip(randint(0, width, n_points),
+                 randint(0, height, n_points))
+    color = (random(), random(), random(), uniform(0.3, 0.6))
+    return {"points": points, "color": color}
 
 
 class Drawing(object):
@@ -41,15 +39,15 @@ class Drawing(object):
         # inititialize cairo drawing
         self.surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
         self.context = cairo.Context(self.surface)
-
         for i in range(conf['min_polies']):
-            self.polies.append(Polygon(width, height, conf['min_poly_points']))
+            self.polies.append(create_random_poly(width,
+                                                  height,
+                                                  conf['min_poly_points']))
 
     def __getstate__(self):
         result = self.__dict__.copy()
         del result['context']
         del result['surface']
-        print result
         return result
 
     def __setstate__(self, dict):
@@ -62,14 +60,15 @@ class Drawing(object):
 
         self.generations += 1
         self.selections.append(self.generations)
-        self.old_polies = new_d = copy.deepcopy(self.polies)
+        self.old_polies = copy.deepcopy(self.polies)
 
         # insert new polygons
         if random() < self.conf['add_poly_rate']:
             rand_idx = randint(0, len(self.polies))
-            self.polies.insert(rand_idx, Polygon(self.w,
-                                                 self.h,
-                                                 self.conf['min_poly_points']))
+            poly = create_random_poly(self.w,
+                                      self.h,
+                                      self.conf['min_poly_points'])
+            self.polies.insert(rand_idx, poly)
 
         # remove polygons
         if random() < self.conf['remove_poly_rate']:
@@ -87,34 +86,34 @@ class Drawing(object):
 
                 # add points
                 if random() < self.conf['add_point_rate']:
-                    rand_idx = randint(0, len(poly.points))
+                    rand_idx = randint(0, len(poly['points']))
                     rand_point = (randint(0, self.w), randint(0, self.h))
-                    poly.points.insert(rand_idx, rand_point)
+                    poly['points'].insert(rand_idx, rand_point)
 
                 # remove a point from the polygon
                 if random() < self.conf['remove_point_rate']:
-                    if len(poly.points) > 3:
-                        poly.points.remove(choice(poly.points))
+                    if len(poly['points']) > 3:
+                        poly['points'].remove(choice(poly['points']))
 
                 # move some of the points
-                for i in range(len(poly.points)):
+                for i in range(len(poly['points'])):
                     if random() < self.conf['move_point_rate']:
                         move = randint(-self.conf['move_point'],
                                         self.conf['move_point'],
                                         2)
-                        x = min(self.w, max(0, poly.points[i][0] + move[0]))
-                        y = min(self.h, max(0, poly.points[i][1] + move[1]))
-                        poly.points[i] = (x, y)
+                        x = min(self.w, max(0, poly['points'][i][0] + move[0]))
+                        y = min(self.h, max(0, poly['points'][i][1] + move[1]))
+                        poly['points'][i] = (x, y)
 
                 # mutate color of polygon
                 if random() < self.conf['color_rate']:
-                    tmp = np.zeros(len(poly.color))
+                    tmp = np.zeros(len(poly['color']))
                     for i in range(3):
                         move = normal(0, self.conf['move_color_std'])
-                        tmp[i] = min(1, max(0, poly.color[i] + move))
+                        tmp[i] = min(1, max(0, poly['color'][i] + move))
                     move = normal(0, self.conf['move_alpha_std'])
-                    tmp[3] = min(0.6, max(0.3, poly.color[3] + move))
-                    poly.color = tuple(tmp)
+                    tmp[3] = min(0.6, max(0.3, poly['color'][3] + move))
+                    poly['color'] = tuple(tmp)
 
     def evaluate(self, other):
         """draw the polygons in a numpy array"""
@@ -126,9 +125,9 @@ class Drawing(object):
         # draw the polygons
         for poly in self.polies:
             self.context.new_path()
-            for point in poly.points + [poly.points[0]]:
+            for point in poly['points'] + [poly['points'][0]]:
                 self.context.line_to(*point)
-            self.context.set_source_rgba(*poly.color)
+            self.context.set_source_rgba(*poly['color'])
             self.context.close_path()
             self.context.fill()
 
@@ -153,3 +152,7 @@ class Drawing(object):
         """print some information on the drawing to the logger"""
         logging.info("Mutation: %d, Selection: %d, error: %d"
                 % (self.generations, len(self.selections), self.errors[-1]))
+
+    def to_json(self):
+        """json representation of the polygons describing the drawing"""
+        print json.dumps(self.polies)
