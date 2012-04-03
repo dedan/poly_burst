af24403203c88759d56e980dee9b0eece2e8cd84
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-03 17:16:32 +0200

    move the sunchronization rectangle to the upper left corner
    because lower left is occupied by windows task bar on the laptop

--- ImageCreatorFeedbackBase.py
+++ ImageCreatorFeedbackBase.py
@@ -58,7 +58,10 @@ class ImageCreatorFeedbackBase(VisionEggFeedback):
 
         # add a blank and the synchronization polygon to the list of polygons
         synchronization_poly = Poly(color = (0, 0, 0, 1.0),
-                                    points = [(10, 10), (20, 10), (20, 20), (10, 20)],
+                                    points = [(-self.width/2 + 10, -self.height/2 + 10),
+                                              (-self.width/2 + 20, -self.height/2 + 10),
+                                              (-self.width/2 + 20, -self.height/2 + 20),
+                                              (-self.width/2 + 10, -self.height/2 + 20)],
                                     position = (0, 0),
                                     size=(self.width, self.height))
         blank_poly = Poly(color = (1.0, 1.0, 1.0, 1.0),
