fabcbcd700ede32eecea3f1705382f51e0e394e4
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-03 11:35:44 +0200

    update training feedback documentation

--- TrainingFeedback.py
+++ TrainingFeedback.py
@@ -18,19 +18,9 @@
     the subject still recognizes a great fraction of the targets, but such
     that the targets are abstract enough.
 
-
-    Gobal variables:
-
-    TRIG_RUN_START: signal the beginning of a run which might consist of many trials.
-    TRIG_RUN_END: signal the end of a run which might consist of many trials.
-    TRIG_TRIAL_START: signal the beginning of a trial.
-    TRIG_TRIAL_END: signal the end of a trial.
-    TRIG_IMG: shift added to the trigger associated to actual images (0=neutral background presented).
-    TRIG_STIM: shift added to the trigger associated to stimuli consisting on polygonal decompositions.
-    TRIG_OK: trigger for Normal activity during non-target stimulus.
-    TRIG_FAKE: trigger for Deviant activity during non-target stimulus.
-    TRIG_MISS: trigger for Normal activity during target stimulus.
-    TRIG_HIT: trigger for Deviant activity during target stimulus.
+    TRIG_IMG, TARGET_BASE, NONTARGET_BASE, POLYGON_BASE are constants
+    which are used to encode target or non-target and the corresponding
+    polygon in the Triggers send to the EEG.
 """
 
 import random as rnd
@@ -65,20 +55,9 @@ class TrainingFeedback(VisionEggFeedback):
         When this is the case, it is indicated.
 
     Variables:
-        folderPath, picsFolder, polyFolder: indicate the path to the corresponding folders.
-        pTarget: probability of displaying target stimuli.
-        stimuliPerTrial: number of stimuli present in a single trial.
-        tryRounds: number of rounds before updating complexity of the stimuli.
-            This can be set up to be not a number of rounds but a moment at which
-            enough taget stimuli have been presented.
-        refTime: refractory time between consecutive target stimuli or between
-            onset of stimuli and target, measured in units with the duration of
-            a single stimulus presentation (in this case: 0.1).
-        numTarget: this variable encode which picture has been chosen as target.
-            In the corresponding folder, pictures are numbered and this should
-            be the corresponding number. This allows for funny data analysis like
-            finding out which pictures prompt more (less) deviant (normal) activity, etc...
-        numNonTarget: list where the numbers of the non-target images are stored.
+        folderPath: indicate the path to the data folders
+        numTarget: which picture has been chosen as target
+        numNonTarget: list of the non-target indeces
         bufferTrigger: this funny buffer is included to correct for some delay
             between the stimulus selection and stimulus onset. The next stimulus
             to be presented is already chosen before the previous one has been
@@ -89,20 +68,10 @@ class TrainingFeedback(VisionEggFeedback):
             handy to have ready all the info which will be conveyed. This is the
             info stored in bufferTrigger, which is just the ID of the stimulus which
             will be displayed.
-        stimQueue: this queue stores the ID's of the different stimuli which have
-            been displayed. They are removed from the queue as soon as the BCI
-            returns the outcome of the classification of the subject's brain
-            activity during the presentation of the corresponding stimulus.
-            OK, Fake, Miss, Hit: count of the events of each case.
         polygonPool: list with lists, each one containing the polygonal decomposition
-            of one of the pictures. This is loaded when the object TF is initialized
-            so that the files are not being accesed every trial.
-        flagRun: logical variable to indicate if the loop should run or stop.
-        trialCount: count of trials.
-        recentTargets: number of recent targets (since last update).
+            of one of the pictures. all file reading is done in the init.
         fullscreen, geometry: variables inherited from poly_feedback (author Stephan).
             May refer to canvas and have been also inherited from superer classes.
-
     """
 
 
@@ -140,7 +109,6 @@ class TrainingFeedback(VisionEggFeedback):
 
     @property
     def geometry(self):
-        """I'm the 'x' property."""
         return self._geometry
 
     @geometry.setter
@@ -150,14 +118,8 @@ class TrainingFeedback(VisionEggFeedback):
         self.height = value[3]
 
     def run(self):
-        """ This function implements the mainloop of this feedback.
+        """ mainloop of this feedback """
 
-            It has been further divided in a run function for each of the different
-            stimuli (images or polygones) that are displayed and also
-            incorporates a call to a function which handles the difficulty
-            of the recognition task (i.e. the similarity between the
-            original image and the polygonal stimuli).
-        """
         # Run starts:
         self.send_parallel(marker.RUN_START)
         l.debug("TRIGGER %s" % str(marker.RUN_START))
@@ -209,13 +171,7 @@ class TrainingFeedback(VisionEggFeedback):
 
 
     def runImg(self):
-        """performs the task of displaying a random image.
-
-            This function creates the 'stimulus_sequence' and attaches to it
-            the corresponding generator (which yields a random image with some
-            neutrum grey background before and after it). The selection of which
-            image is to be displayed is left to this generator.
-        """
+        """ display an image selected by the prepareImg generator """
 
         # Adding an image and the generator:
         generator = self.prepareImg()
@@ -225,12 +181,7 @@ class TrainingFeedback(VisionEggFeedback):
 
 
     def runPoly(self):
-        """ performs the task of displaying the polygonal stimuli.
-            This function creates the 'stimulus_sequence' and attaches to it
-            the corresponding generator (which yields successive selections of
-            polygonal decompositioins). The selection of whether the displayed
-            stimuli are target or not is left to this generator.
-        """
+        """ display a polygon selected by the preparePoly generator """
 
         # Creating ManyPoly objects to load the different polygons to be displayed:
         listPoly = [Poly(color = (0, 0, 0, 1.0), # Set the target color (RGBA) black
@@ -240,7 +191,10 @@ class TrainingFeedback(VisionEggFeedback):
                          size=(self.width, self.height)),
                     Poly(color = (1.0, 1.0, 1.0, 1.0), # Set the target color (RGBA) black
                          orientation = 0.0,
-                         points = [(-self.width, -self.height), (-self.width, self.height), (self.width, self.height), (self.width, -self.height)],
+                         points = [(-self.width, -self.height),
+                                   (-self.width, self.height),
+                                   (self.width, self.height),
+                                   (self.width, -self.height)],
                          position = (self.width/2, self.height/2),
                          size=(self.width, self.height))]
         target = ManyPoly(listPoly, size=(self.width, self.height))
@@ -257,7 +211,7 @@ class TrainingFeedback(VisionEggFeedback):
 
 
     def prepareImg(self):
-        """ generator yields when the setting for a new image to be presented have been prepared.
+        """ generator yields when the setting for a new image have been prepared.
 
             It just selects whether to display the target image or a neutrum
             background. The selection of the target image is left to the
@@ -299,7 +253,8 @@ class TrainingFeedback(VisionEggFeedback):
 
         self.numNonTarget = range(1,len(self.dictImgNames)+1)
         self.numTarget = self.numNonTarget.pop(rnd.randint(0,len(self.numNonTarget)-1))
-        l.debug('Target Image: ' + str(self.numTarget) + 'Name: ' + self.dictImgNames[self.numTarget])
+        l.debug('Target Image: ' + str(self.numTarget) +
+                'Name: ' + self.dictImgNames[self.numTarget])
         l.debug('NonTarget Images: ' + str(self.numNonTarget))
         self.bufferTrigger = TRIG_IMG + self.numTarget
         info = json.load(open(os.path.join(self.folderPath,
@@ -310,7 +265,7 @@ class TrainingFeedback(VisionEggFeedback):
 
 
     def preparePoly(self):
-        """ generator yields when the desired polygons have been loaded in the ManyPoly stimulus.
+        """ generator yields when the desired polygons have been loaded
 
             It handles the selection of target or non-target stimulus, but
             loading the desired polygonal decomposition into 'self.manyPoly'
@@ -358,7 +313,7 @@ class TrainingFeedback(VisionEggFeedback):
 
 
     def preparePolyDecomp(self, blank=False):
-        """ loads the information stored in a polygonal decomposition into the object 'self.manyPoly'.
+        """ loads the information stored in a polygonal decomposition into 'self.manyPoly'.
 
             This object is the one which is displayed, so this step basically
             prepares the information which will be drawn into the canvas.
