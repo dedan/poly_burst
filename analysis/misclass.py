30963aeda350b1b911ebd10e6abfbdc812e02da9
Stephan Gabler <stephan.gabler@gmail.com>
2012-07-09 12:06:34 +0200

    better idea of how to do analysis
    I realized that I collected the wrong information previously. This commit is just to store the current state of the script. maybe I'll need some of the regexes again

--- analysis/misclass.py
+++ analysis/misclass.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
+what to do:
+for each object and for each of its polygons, show the real target and what
+the classifier selected.
 
-Created by  on 2012-01-27.
-Copyright (c) 2012. All rights reserved.
+I need to parse the following information
+
+for each object
+    * non_targets
+    for each burst
+        * non_target_image --> non_target_polygon mapping
+
 """
 
 import os
@@ -22,8 +30,8 @@ current_path = os.path.join(data_folder, VP_CODE)
 ### first analysis: make a plot of pairs -> missclassified vs. correct stimulus
 
 # get classifier output from the bbci_apply_log.txt
+p = re.compile('i:cl_output=\d')
 with open(os.path.join(current_path, 'bbci_apply_log.txt')) as f:
-    p = re.compile('i:cl_output=\d')
     res = p.findall(f.read())
     cl_output = [int(r[-1]) for r in res]
 
@@ -32,37 +40,63 @@ with open(os.path.join(current_path, 'bbci_apply_log.txt')) as f:
 object_sep = re.compile('Target Image: (\d+) Name: (\d+)')
 burst_sep = re.compile('Building and presenting polygonal stimuli')
 polygon_selected = re.compile('Polygon (\d+) selected for display')
+stimulus_selected = re.compile('(TARGET|NONTARGET) (\d+) s?selected')
 
-objects, cur_burst = [], []
+objects, cur_burst, cur_stim = [], [], []
 obj_name = {}
 with open(os.path.join(current_path, log_name)) as f:
-    for line in f.readlines():
+    for line in f:
 
         new_object = object_sep.search(line)
         if new_object:
-            cur_obj = {"name": new_object.groups()[1], "bursts": []}
+            cur_obj = {"name": new_object.groups(), "polies": [], "stims": []}
             objects.append(cur_obj)
             obj_name[int(new_object.groups()[0])] = new_object.groups()[1]
 
         new_burst = burst_sep.search(line)
         if new_burst:
             if cur_burst:
-                cur_obj["bursts"].append(cur_burst)
+                cur_obj["polies"].append(cur_burst)
                 cur_burst = []
+                cur_obj["stims"].append(cur_stim)
+                cur_stim = []
+
 
         new_poly = polygon_selected.search(line)
         if new_poly:
-            cur_burst.append(new_poly.groups())
-    cur_obj["bursts"].append(cur_burst)
+            cur_burst.append(int(new_poly.groups()[0]))
+
+        new_stim = stimulus_selected.search(line)
+        if new_stim:
+            print new_stim.groups()
+            cur_stim.append(int(new_stim.groups()[1]))
+
+    cur_obj["polies"].append(cur_burst)
+    cur_obj["stims"].append(cur_stim)
 
 
-# plot for each object each target and its classification
-for obj in [objects[0]]:
-    for i, burst in enumerate(obj['bursts']):
-        plt.figure()
-        plt.subplot(121)
-        fname = os.path.join(stimuli_folder, obj['name'], 'decomp', 'decomp_' + str(i) + '.png')
-        plt.imshow(plt.imread(fname))
+# # plot for each object each target and its classification
+# fname_pattern = os.path.join(stimuli_folder, '%(stim)s', 'decomp', 'decomp_' + '%(poly)s' + '.png')
+# c = 0
+# for obj in [objects[0]]:
+
+#     non_targets = range(1, n_images+1)
+#     non_targets.remove(int(obj['name'][0]))
+
+#     for i, burst in enumerate(obj['polies']):
+#         plt.figure()
+#         plt.subplot(121)
+#         fname = fname_pattern % {'stim': obj['name'][1], 'poly': str(i)}
+#         plt.imshow(plt.imread(fname))
+
+#         plt.subplot(122)
+#         if cl_output[c] == int(obj['name'][0]):
+#             fname = fname_pattern % {'stim': obj['name'][1], 'poly': str(i)}
+#         else:
+#             fname = fname_pattern % {'stim': obj_name[cl_output[c]], 'poly': str(burst[''])}
+
+
+#         c += 1
 
 
 
